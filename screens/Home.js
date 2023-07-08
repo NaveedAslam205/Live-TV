914ce226fad50958ec10bd44bc477c04dec47963
NaveedAslam205 <128507574+NaveedAslam205@users.noreply.github.com>
2023-07-08 11:53:23 +0500

    Menu Completed.
    Channel type selection tab menu implemented.

--- screens/Home.js
+++ screens/Home.js
@@ -7,6 +7,7 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  ActivityIndicator
 } from "react-native";
 import { Video, ResizeMode } from "expo-av";
 import YoutubePlayer from "react-native-youtube-iframe";
@@ -29,6 +30,12 @@ export default function Home(props) {
   const [status, setStatus] = useState({});
   const [chanels, setChanels] = useState([]);
   const [currentChanel, setCurrentChanel] = useState([]);
+
+  const [chanelType, setChanelType] = useState(`News`);
+  const [pressed, setPressed]=useState('News');
+
+  const [loading, setLoading] = useState(false);
+  
 
   const db = getFirestore(app);
 
@@ -110,13 +117,62 @@ export default function Home(props) {
       }}
     /> */}
 
+    <View style={styles.menu }>
+        <TouchableOpacity
+          onPress={() => {setChanelType('News'),setPressed('News')}}
+          style={pressed=='News' ? styles.touched : styles.touchabble}
+        >
+          <Image
+            source={require("../assets/news.png")}
+            style={styles.menuImages}
+          ></Image>
+          <Text style={styles.menuText}>News</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => {setChanelType('Sports'),setPressed('Sports')}}
+          style={pressed=='Sports' ? styles.touched : styles.touchabble}
+        >
+          <Image
+            source={require("../assets/sports.png")}
+            style={styles.menuImages}
+          ></Image>
+          <Text style={styles.menuText}>Sports</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => {setChanelType('Entertainment'),setPressed('Entertainment')}}
+          style={pressed== 'Entertainment' ? styles.touched : styles.touchabble}
+        >
+          <Image
+            source={require("../assets/entertainment.png")}
+            style={styles.menuImages}
+          ></Image>
+          <Text style={styles.menuText}>Entertainment</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => {setChanelType('Geography'),setPressed('Geography')}}
+          style={pressed== 'Geography' ? styles.touched : styles.touchabble}
+        >
+          <Image
+            source={require("../assets/geo.png")}
+            style={styles.menuImages}
+          ></Image>
+          <Text style={styles.menuText}>Geography</Text>
+        </TouchableOpacity>
+      </View>
+      {loading ? <ActivityIndicator color={'#264653'} /> : null}
+
       <View style={styles.list}>
         <FlatList
           data={chanels}
           numColumns={2}
           renderItem={({ item }) => {
+
             return (
-              <TouchableOpacity
+              chanelType==item.type ?
+             <TouchableOpacity
                 onPress={() => {
                   setCurrentChanel(item);
                 }}
@@ -124,8 +180,9 @@ export default function Home(props) {
               >
                 <Image source={{ uri: item.img }} style={styles.image}></Image>
                 <Text style={styles.txt}>Live</Text>
-              </TouchableOpacity>
+              </TouchableOpacity> :null
             );
+            
           }}
         />
       </View>
@@ -204,5 +261,47 @@ const styles = StyleSheet.create({
     right: 15,
     fontSize: 14,
     fontWeight: "bold",
+  },
+  menu: {
+    height: 50,
+    width:350,
+    flexDirection: "row",
+    backgroundColor: "#0000000",
+    margin:10,
+    marginLeft:10,
+    marginRight:10,
+    borderRadius:10,
+    paddingLeft:10,
+    paddingRight:10,
+    justifyContent:'center',
+    alignSelf:'center',
+    backgroundColor:'#264653',
+  },
+
+  menuImages: {
+    height: 20,
+    width: 20,
+    alignSelf: "center",
+  },
+  menuText: {
+    fontSize: 12,
+    color: "#ffff",
+    fontWeight: "bold",
+  },
+  touchabble: {
+    flexDirection: "column",
+    marginLeft: 15,
+    marginRight: 15,
+    marginTop: 8
+  },
+  touched: {
+    flexDirection: "column",
+    marginLeft: 15,
+    marginRight: 15,
+    marginBottom:8,
+    marginTop: 8,
+    borderWidth:0.5,
+    borderRadius:3,
+    borderColor:'white'
   },
 });
